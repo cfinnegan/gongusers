d5b1b8d22529b20eeb6d014a741fc6509cb25c86
Ciaran Finnegan <ciaran.finnegan3@gmail.com>
2025-11-25 17:07:30 +0000

    Cleaning up unused files

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,10 +1,8 @@
-import { useAuth } from '../../auth/AuthContext';
 import type { User } from '../../types/users';
 import ProfileImage from './ProfileImage';
 import ProfileName from './ProfileName';
 
 function Profile({ user, isLight }: { user: User | null; isLight?: boolean }) {
-  const {} = useAuth();
   return (
     <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
       <ProfileImage user={user} />
